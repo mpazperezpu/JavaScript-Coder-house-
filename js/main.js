a4374238bb957a550a53772ca9289667f1bd39e0
mpazperezpu <mariadelapazperezpuentes@Marias-Air.fibertel.com.ar>
2024-11-08 19:13:23 -0300

    Primera Entrega

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
 let precioEconomico=6500;
 let precioPremium =8000;
-let total ;
 let tipo;
 let cantidad;
+
+
+function tipoCatering(){
 while(true){
     tipo = prompt("Ingrese el tipo de catering que desea: \n Economico \n Premium").toLowerCase();
     if (tipo ==="economico" || tipo === "premium"){
-        console.log(tipo)
-    break;
+    return tipo;
     } else {
     alert(' Por favor ingrese una opcion valida');
 }
-}
+}}
+function cantidadPersonas (){
 while(true){
     cantidad = parseInt(prompt('Para cuantas personas desea el catering?'));
     if(!isNaN(cantidad)){
-        break;
+        return cantidad;
     }else{
         alert("Por favor ingrese un numero");
     }
-}
+}}
+function calcularTotal(tipo,cantidad){
 if (tipo === "economico"){
- total=cantidad*precioEconomico
+    return cantidad*precioEconomico;
 }
 if(tipo === "premium"){
-    total =cantidad*precioPremium
-}
+    return cantidad*precioPremium;
+}}
 
-
+tipo = tipoCatering()
+cantidad = cantidadPersonas()
+ let total = calcularTotal( tipo,cantidad)
 alert(`El precio de un catering ${tipo} para ${cantidad} personas tendria un costo de ${total}`)
