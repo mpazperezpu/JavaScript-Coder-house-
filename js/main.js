60eb51320bd878117c3baa5c512b67552dd6ab12
mpazperezpu <mariadelapazperezpuentes@Marias-Air.fibertel.com.ar>
2024-11-19 15:21:32 -0300

    Segunda Entrega

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let precioEconomico=6500;
 let precioPremium =8000;
 let tipo;
@@ -33,4 +34,38 @@ if(tipo === "premium"){
 tipo = tipoCatering()
 cantidad = cantidadPersonas()
  let total = calcularTotal( tipo,cantidad)
-alert(`El precio de un catering ${tipo} para ${cantidad} personas tendria un costo de ${total}`)
+alert(`El precio de un catering ${tipo} para ${cantidad} personas tendria un costo de ${total}`)*/
+
+const productos=[
+    {   
+        id: 1,
+        nombre:"Catering Economico",
+        precio: 6500,
+    },
+    {
+        id: 2,
+        nombre:"Catering Premium",
+        precio: 8000,
+    },
+    {
+        id:3,
+        nombre:"Pizza Party",
+        precio : 7900,
+    },
+    {
+        id:4,
+        nombre: "Docena de Empanadas Congeladas",
+        precio: 9500,
+    }
+
+]
+let buscarProductos = prompt('Ingrese el producto que esta buscando').toLowerCase();
+let resultado = productos.filter((producto) => producto.nombre.toLowerCase().includes(buscarProductos));
+if (resultado.length >0){
+    let productosEncontrados= "Hemos encontrado los siguientes productos:\n"+
+        resultado.map((producto) =>  `${producto.nombre} $${producto.precio}`).join('\n')
+        alert(productosEncontrados)
+    }
+else{
+    alert("No hemos encontrado ningu producto")
+}
