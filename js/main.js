c60489fdc48d732f1963eeee198f8dfaecf5ece0
mpazperezpu <mariadelapazperezpuentes@Marias-Air.fibertel.com.ar>
2024-12-05 11:29:13 -0300

    Tercer Entrega

--- js/main.js
+++ js/main.js
@@ -38,34 +38,68 @@ alert(`El precio de un catering ${tipo} para ${cantidad} personas tendria un cos
 
 const productos=[
     {   
-        id: 1,
-        nombre:"Catering Economico",
-        precio: 6500,
+        id: 1,nombre:"Catering Economico",precio: 6500,
     },
     {
-        id: 2,
-        nombre:"Catering Premium",
-        precio: 8000,
+        id: 2,nombre:"Catering Premium",precio: 8000,
     },
-    {
-        id:3,
-        nombre:"Pizza Party",
-        precio : 7900,
+    {   id:3,nombre:"Pizza Party Economico",precio : 6900,
     },
-    {
-        id:4,
-        nombre: "Docena de Empanadas Congeladas",
-        precio: 9500,
+    {   id:4,nombre:"Pizza Party Premium",precio : 7900,
+    },
+    {   id:5,nombre: "Docena de Empanadas Congeladas",precio: 9500,
     }
-
-]
-let buscarProductos = prompt('Ingrese el producto que esta buscando').toLowerCase();
-let resultado = productos.filter((producto) => producto.nombre.toLowerCase().includes(buscarProductos));
-if (resultado.length >0){
-    let productosEncontrados= "Hemos encontrado los siguientes productos:\n"+
-        resultado.map((producto) =>  `${producto.nombre} $${producto.precio}`).join('\n')
-        alert(productosEncontrados)
-    }
-else{
-    alert("No hemos encontrado ningu producto")
+];
+const containerProducts = document.getElementById('productos');
+productos.forEach(producto =>{
+    const productoDiv = document.createElement('div');
+    productoDiv.innerHTML = `
+    <h3>${producto.nombre}</h3>
+    <p>Precio: $${producto.precio}</p>
+    <button id='agregar-${producto.id}'>Agregar al Carrito</button>`;
+    containerProducts.appendChild(productoDiv);
+    const button = document.getElementById(`agregar-${producto.id}`);
+    button.addEventListener('click',()=>
+    {
+        agregarAlCarrito(producto);
+    });
+    
+});
+let carrito =[]
+function agregarAlCarrito(producto){
+    carrito.push(producto)
+    console.log(carrito)
+    
 }
+const containerCarrito = document.getElementById('carrito');
+const totalCarrito = document.getElementById('total');
+function actualizarCarrito(){
+    containerCarrito.innerHTML = '';
+    carrito.forEach(producto => {
+        const li = document.createElement('li');
+        li.textContent = `${producto.nombre} - ${producto.precio}`;
+        containerCarrito.appendChild(li);
+    })
+    const total = carrito.reduce ((acc,producto)=> acc + producto.precio ,0)
+    totalCarrito.textContent = `Total:${total}`;
+}
+function agregarAlCarrito(producto){
+    carrito.push(producto);
+    actualizarCarrito();
+}
+function guardarLocalStorage(){
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+function agregarAlCarrito(producto){
+    carrito.push(producto);
+    actualizarCarrito();
+    guardarLocalStorage();
+}
+function cargarCarrito(){
+    const carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado){
+        carrito = JSON.parse(carritoGuardado);
+        actualizarCarrito();
+    }
+}
+cargarCarrito();
